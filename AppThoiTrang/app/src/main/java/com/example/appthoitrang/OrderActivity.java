741b1d8e743a5332cbbb95f5f51840edc2c77c26
vnext04 <tranquangdao25102001@gmail.com>
2022-11-18 16:34:32 +0700

    cart

--- AppThoiTrang/app/src/main/java/com/example/appthoitrang/OrderActivity.java
+++ AppThoiTrang/app/src/main/java/com/example/appthoitrang/OrderActivity.java
@@ -1,16 +1,65 @@
 package com.example.appthoitrang;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.appthoitrang.Models.CustomerMangment;
 import com.example.appthoitrang.R;
+import com.squareup.picasso.Downloader;
+
+import java.util.HashMap;
 
 public class OrderActivity extends AppCompatActivity {
-
+private RecyclerView recyclerView ;
+CustomerMangment customerMangment;
+private final Response.Listener<String> listener = new Response.Listener<String>() {
+    @Override
+    public void onResponse(String response) {
+        try {
+            Log.e("Bkap", "JSON: "+response);
+        }catch (Exception e){}
+    }
+};
+    private Response.ErrorListener errorListener= new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.e("BKAP","ERROR: " + error.getMessage());
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Toolbar toolbar = findViewById(R.id.itemToolBarCategory);
+        toolbar.setTitle("History orders");
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        customerMangment = new CustomerMangment(this);
+//        loadJSONOrder();
+        recyclerView=findViewById(R.id.listOrder);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    private void loadJSONOrder() {
+        HashMap<String,String> cus= customerMangment.myaccount();
+        String mEmail = cus.get(customerMangment.EMAIL);
+        Host host = new Host();
+        String url = "http://"+host.getHost()+":8183/api/list_order?email=" +mEmail;
+        RequestQueue queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(url, listener, errorListener);
+
+        queue.add(stringRequest);
     }
 }
--- AppThoiTrang/app/src/main/java/com/example/appthoitrang/ProfileFragment.java
+++ AppThoiTrang/app/src/main/java/com/example/appthoitrang/ProfileFragment.java
@@ -1,16 +1,42 @@
 package com.example.appthoitrang;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.appthoitrang.R;
 
-public class ProfileFragment extends AppCompatActivity {
+public class ProfileFragment extends Fragment {
+private static final String ARG_PARAM1 = "param1";
+private static final String ARG_PARAM2 = "param2";
 
+private String mParam1;
+private String mParam2;
+
+    public ProfileFragment() {
+    }
+
+    public static ProfileFragment newInstance (String param1, String param2){
+        ProfileFragment profileFragment = new ProfileFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString(ARG_PARAM1,param1);
+        bundle.putString(ARG_PARAM1,param2);
+        profileFragment.setArguments(bundle);
+        return profileFragment;
+    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_fragment);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+    public View onCreateView(LayoutInflater inflater , ViewGroup container , Bundle bundle){
+        return inflater.inflate(R.layout.profile_fragment,container,false);
     }
 }
